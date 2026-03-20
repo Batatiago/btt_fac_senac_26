35da463cf9259b17bc6fcf428f1bf868c5fdb5dc
Tiago Andrade Lima <tiago.andrade.lima@gmail.com>
2026-03-20 10:19:45 -0300

    lista-2 de python atualizada

--- Python/Lista de Exercicios - 2/ex8.py
+++ Python/Lista de Exercicios - 2/ex8.py
@@ -1,9 +1,10 @@
 numeros = []
 
-for i in range(5):
-    valor = int(input(f"Digite o {i+1}º número inteiro: "))
+for index in range(5):
+    valor = int(input(f"Digite o {index+1}º número inteiro: "))
     numeros.append(valor)
 
 print("\nÍndice e valor dos números:")
-for indice, numero in enumerate(numeros):
-    print(f"Posição {indice}: {numero}")
+
+for index, numero in enumerate(numeros):
+    print(f"Posição {index}: {numero}")
