988a10fc4d3c6962850f4041cd5c0f9d0adb8867
Tiago Andrade Lima <tiago.andrade.lima@gmail.com>
2026-03-23 10:34:58 -0300

    exemplos de slices usando numpy e palindromo

---
+++ Python/Arrays - Slices/exemplos.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+num = np.random.randint(10)
+print(num)
+
+mega = np.random.randint(1,61,size=(6,6))
+print(mega)
---
+++ Python/Arrays - Slices/palindromo.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+pa = 'arara'
+
+print(f"{pa} e palindromo" if pa == pa[::-1] else f"{pa} nao e palindromo")
