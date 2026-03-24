6118bdb70154cadae91ac71cddf6279714fcc640
Tiago de Andrade Lima <tiago.andrade.lima@gmail.com>
2026-03-24 15:57:49 -0300

    exercicios 11 e 12

--- Python/Arrays - Slices/exemplos.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-num = np.random.randint(10)
-print(num)
-
-mega = np.random.randint(1,61,size=(6,6))
-print(mega)
--- Python/Arrays - Slices/palindromo.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-pa = 'arara'
-
-print(f"{pa} e palindromo" if pa == pa[::-1] else f"{pa} nao e palindromo")
